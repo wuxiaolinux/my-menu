165da60b5915845dba950e9a88938b84361c1776
wuxiaolinux@163.com <wuxiaolinux@163.com>
2015-12-15 20:40:03 +0800

    调整一些细节

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 $(function() {
-	$('.tj_menu_ul_li_click').on('click', function() {
+	$('.tj_menu_click').on('click', function() {
 		openMenu(this);
 	});
+
+	$('.fun_leaf').on('click', function() {
+		openFun(this);
+	})
 });
 
 function openMenu(obj) {
@@ -15,5 +19,8 @@ function openMenu(obj) {
 		$(obj).children('.glyphicon').removeClass('glyphicon-folder-open').addClass('glyphicon-folder-close');
 		$(obj).attr('status', 'menu_close');
 	}
-	
+}
+
+function openFun(obj) {
+	alert('打开功能啦！');
 }
